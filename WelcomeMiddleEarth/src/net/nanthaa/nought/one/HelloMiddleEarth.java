5ebee3c0c9dc9cd815ef81519b6f1ba714a86adf
nanthaa-01 <email.nanthaa@gmail.com>
2017-08-05 13:15:11 +0530

    code alligned

--- WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
+++ WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
@@ -1,13 +1,17 @@
 package net.nanthaa.nought.one;
 
-public class HelloMiddleEarth {
+public class HelloMiddleEarth
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		String names[] = { "Anand", "Nanthaa", "Saran", "Shyam", "Vicky" };
 
 		// print greetings to the fine folks in the Middle Earth
 		for (String name : names)
+		{
 			System.out.format("Hello, %s! \n", name);
+		}
 		System.out.println(" How are u doing today ? ");
 	}
 
