1c6aa064746f05f49e397a4f5241eb76fd568b25
nanthaa-01 <email.nanthaa@gmail.com>
2017-08-05 12:57:05 +0530

    converted for loop

--- WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
+++ WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
@@ -3,13 +3,14 @@ package net.nanthaa.nought.one;
 public class HelloMiddleEarth
 {
 
+	// print greetings to the fine folks in the Middle Earth
 	public static void main(String[] args)
 	{
 		String names[] = { "Anand", "Nanthaa", "Saran", "Shyam" };
 
-		for (int i = 0; i < names.length; i++)
+		for (String name : names)
 		{
-			System.out.format("Hello, %s! \n", names[i]);
+			System.out.format("Hello, %s! \n", name);
 		}
 
 	}
