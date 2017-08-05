72b74b555fb02f1ed6dddaa27ace40e17c6f03bd
nanthaa-01 <email.nanthaa@gmail.com>
2017-08-05 12:43:40 +0530

    new project added

---
+++ WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
@@ -0,0 +1,17 @@
+package net.nanthaa.nought.one;
+
+public class HelloMiddleEarth
+{
+
+	public static void main(String[] args)
+	{
+		String names[] = { "Anand", "Nanthaa", "Saran", "Shyam" };
+
+		for (int i = 0; i < names.length; i++)
+		{
+			System.out.format("Hello, %s! \n", names[i]);
+		}
+
+	}
+
+}
