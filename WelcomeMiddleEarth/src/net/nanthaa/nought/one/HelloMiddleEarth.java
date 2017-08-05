491832e8ee63569873eed2f5f06c56c4cde21879
nanthaa-02 <email.nanthaa@gmail.com>
2017-08-05 13:03:02 +0530

    added a print out and one charactor

--- WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
+++ WelcomeMiddleEarth/src/net/nanthaa/nought/one/HelloMiddleEarth.java
@@ -1,15 +1,13 @@
 package net.nanthaa.nought.one;
 
-public class HelloMiddleEarth
-{
+public class HelloMiddleEarth {
 
-	public static void main(String[] args)
-	{
-		String names[] = { "Anand", "Nanthaa", "Saran", "Shyam" };
+	public static void main(String[] args) {
+		String names[] = { "Anand", "Nanthaa", "Saran", "Shyam", "Vicky" };
 
-		for (int i = 0; i < names.length; i++)
-		{
+		for (int i = 0; i < names.length; i++) {
 			System.out.format("Hello, %s! \n", names[i]);
+			System.out.println(" How are u doing today ? ");
 		}
 
 	}
